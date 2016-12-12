3d9fb6f288ab005c4be598bb78172a27e2c5bfd4
Guilhem Brouat <guilhem.brouat@docdoku.com>
2016-12-12 14:43:56 +0100

    Simplify code

--- missions/launch_to_orbit.py
+++ missions/launch_to_orbit.py
@@ -6,14 +6,9 @@ from lib.mission import Mission
 from lib.steps.launch import all_steps
 
 
-def run():
-  """Run mission"""
+if __name__ == "__main__":
   conn = krpc.connect()
   mission = Mission(conn, all_steps).run()
 
   while next(mission):
     time.sleep(0.01)
-
-
-if __name__ == "__main__":
-  run()
